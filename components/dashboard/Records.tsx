79a54dcb6ff15b64c7c33dd94853069804b40712
Kunal <me@kunal.sh>
2019-11-23 14:35:25 -0800

    init csv records

--- components/dashboard/Records.tsx
+++ components/dashboard/Records.tsx
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  Avatar,
-  Button,
-  TextField,
-  Link,
-  Grid,
-  Typography,
-  Container
-} from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { makeStyles } from "@material-ui/core/styles";
 import { gql } from "apollo-boost";
